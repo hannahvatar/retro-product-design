eb0197c9776d460642603ac30407b65c8f82f040
Anabelle Dubois <anabelle.dubois@icloud.com>
2025-03-22 16:10:29 -0400

    Added ebook and audiobook features

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+  get "podcast", to: "pages#podcast"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
